d86c357f39916b0775bca37e6a0fa154992a23cc
ramadoni16 <donicyberit@gmail.com>
2022-11-08 16:56:04 +0700

    first commit

---
+++ popup.js
@@ -0,0 +1,33 @@
+// var x = 15;
+// console.log("Hello World!");
+// console.log("isi dari variabel x adalah " + x);
+
+// PopUp Box
+
+// 1. alert
+// alert("Hello World!");
+
+// 2. prompt
+// var nama = prompt("Masukkan  nama :");
+// alert(nama);
+
+// 3. confirm
+// var tes = confirm("Kamu Yakin??");
+// alert(tes);
+// var tes = confirm("Kamu Yakin ?");
+// if (tes === true) {
+//   alert("User Menekan OK!");
+// } else {
+//   alert("User Menekan CANCEL!");
+// }
+
+// alert("selamat Datang");
+// var lagi = true;
+
+// while (lagi === true) {
+//   var nama = prompt("Masukkan Nama : ");
+//   alert("halo" + nama);
+
+//   lagi = confirm("Coba Lagi ?");
+// }
+// alert("terimakasih");
