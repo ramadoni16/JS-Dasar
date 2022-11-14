b8e98c791c3b63a015e045267b9e3a87f7c4b617
ramadoni16 <donicyberit@gmail.com>
2022-11-14 13:27:39 +0700

    While

---
+++ While.js
@@ -0,0 +1,17 @@
+// proses looping Terus Menerus
+// while (true) {
+//   console.log("Hello World!!!");
+// }
+
+///////////////////////////
+// var ulang = true;
+// while (ulang) {
+//   console.log("Hello World!!!");
+//   ulang = confirm("Lagi ?");
+// }
+
+var nilaiAwal = 1;
+while (nilaiAwal <= 10) {
+  console.log("Hello World " + nilaiAwal + "x");
+  nilaiAwal++;
+}
