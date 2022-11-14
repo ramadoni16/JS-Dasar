f45652c2e6c40ca281505d6a1561cdb0d1102451
ramadoni16 <donicyberit@gmail.com>
2022-11-14 13:56:30 +0700

    Latihan (While)

--- angkot1.js
+++ angkot1.js
@@ -1,6 +1,13 @@
 // Latihan While
-var angkot = 1;
-while (angkot <= 10) {
-  console.log("Angkot No. " + angkot + " beroperasi dengan baik.");
-  angkot++;
+// var angkot = 1;
+// while (angkot <= 10) {
+//   console.log("Angkot No. " + angkot + " beroperasi dengan baik.");
+//   angkot++;
+// }
+
+var jmlAngkot = 10;
+var noAngkot = 1;
+while (noAngkot <= jmlAngkot) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+  noAngkot++;
 }
