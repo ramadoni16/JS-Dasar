0380378b9d2ef4863b27b821be5b4f147c7455a4
ramadoni16 <donicyberit@gmail.com>
2022-11-15 11:20:17 +0700

    IF-else & else-if

---
+++ if-else.js
@@ -0,0 +1,16 @@
+// var angka = prompt("masukkan angka:");
+
+// if (angka % 2 == 0) {
+//   alert(angka + " adalah bilangan GENAP");
+// } else {
+//   alert(angka + " adalah bilangan GANJIL");
+// }
+
+var angka = prompt("masukka angka:");
+if (angka % 2 === 0) {
+  alert(angka + " adalah bilangan Genap");
+} else if (angka % 2 === 1) {
+  alert(angka + "adalah bilangan Ganjil");
+} else {
+  alert("yang anda masukkan bukan angka!!");
+}
