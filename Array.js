ac076e48bc325e1bfc97a0b1543f5e3deba755eb
ramadoni16 <donicyberit@gmail.com>
2022-11-17 11:08:53 +0700

    array

---
+++ Array.js
@@ -0,0 +1,7 @@
+// Susunan sistematis dari objek-objek yang serupa tipe data yang digunakan untuk
+// mendeskripsikan kumpulan elemen (nilai atau variabel),yang tiap-tiap elemennya memiliki index.
+// var hari = ["senin", "selasa", "rabu"];
+
+// Array Bersarang
+var myArr3 = ["teks", 2, false, "myFunc", [4, 5, 6]];
+console.log(myArr3[4][1]);
