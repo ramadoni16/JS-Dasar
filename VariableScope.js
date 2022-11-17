a64b062753d0ea9485b8906d24e882ec0fcb1278
ramadoni16 <donicyberit@gmail.com>
2022-11-17 09:43:40 +0700

    Variable Scope

---
+++ VariableScope.js
@@ -0,0 +1,12 @@
+// block scope{} (c & java)
+
+// function Scope (js)
+// java script tidak menganut block scope
+
+// Global scope / window scope
+var a = 1;
+function tes() {
+  //   console.log(a);
+}
+tes(a);
+console.log(a);
